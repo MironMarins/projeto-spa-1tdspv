3e9ea3edb2b03ca08461ec59640f6442336856f5
MironMarins <rm551801@fiap.com.br>
2023-09-19 22:40:11 -0300

    acompanhando o professor 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,16 @@
-import reactlogo from "./assets/react.svg";
-import Cabecalho from "../aula-rotas/src/components/cabecalho";
-import Conteudo from "../aula-rotas/src/components/Conteudo";
+import { Outlet} from "react-router-dom";
+import Cabecalho from "./components/Cabecalho";
+import Rodape from "./components/Rodape";
+
 export default function App() {
   
-  
-  //área declaratica
 
-  let reactlogoTextoAlt = "Logo do React"
+
   return (
     <>
-    {/* Área imperativa! */}
-
-    
-      {/* UM CABEÇALHO COM UM HEADER, um h1, uma lista ul com 3 itens com 3 links. */}
-        <Cabecalho/> 
-      {/* uma seção com uma Div com 3 paragrafos, com 3 linhas de lorem, uma imagem*/}
-      <Conteudo reacLogoProps={reactLogo} reactLogoTextoAltProps={reactLogoTextoAlt}/>
-      
-      {/* Um rodapé com uma div, uma lista ul com 3 itens e links para redes sociaiis*/}
+      <Cabecalho/>
+        <Outlet/>
       <Rodape/>
-    
     </>
   )
 }
---
+++ src/routes/Home.jsx
@@ -0,0 +1,12 @@
+
+export default function Home() {
+
+    document.title = "Home";
+  
+    return (
+      <div>
+          <h1>COMPONENTE - HOME</h1>
+      </div>
+    )
+  }
+  
